609bed6ad48639aac4a36f7a7b85954bd221522c
Scott Miller <wsmiller324@gmail.com>
2015-06-26 15:00:24 -0700

    added test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Scotty on 6/26/15.
+//  Copyright (c) 2015 Scott Miller. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Scotty on 6/26/15.
+//  Copyright (c) 2015 Scott Miller. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
